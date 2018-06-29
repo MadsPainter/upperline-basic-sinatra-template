fc26598643d3d10b81b80f29d8cc41bf9dade124
MadsPainter <paintermadeline@gmail.com>
2018-06-29 16:09:06 +0000

    this is my first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,43 @@ require './config/environment'
 require './app/models/sample_model'
 
 class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+    configure do
+      set :public_folder, 'public'
+      set :views, 'app/views'
+    end
+  
+    get '/' do
+      erb :the_homepage
+    end
+    
+    get '/empowering-words' do
+      erb :role_model
+    end
+    
+    get '/gallery' do
+      erb :gallery
+    end
+    
+    get '/need-help' do 
+      erb :needhelp
+    end
+    
+    post '/submission' do
+      erb :submission
+    end
+end 
 
-  get '/' do
-    return erb :index
-  end
-end
+  
+    # if @new_name.include?(@pseudonym)
+      
+    # else
+    #   Pseudonym.new(params[:pseudonym])
+      
+    # end
+    
+    # @name = params[:name]
+    # @content1 = params[:content1]
+    # @content2 = params[:content2]
+    # Submission.new(@name, @content1, @content2)
+    # erb :submission
+
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,18 @@
-class SampleModel
-  attr_accessor :name, :age
-end
+class Submission
+  attr_accessor :name, :content1, :content2
+  @@count = 0
+  @@gallery = {}
+  def initialize(name, content1, content2)
+      @name = name
+      @content1 = content1
+      @content2 = content2 
+      @@count += 1
+      @@gallery [:name] = [:content1, :content2]
+  end
+  
+  def self.new_name
+    @@gallery
+  end
+end 
+
+# first_instance = Submission.new("unicorns", "Have faith in yourself.", "http://3.bp.blogspot.com/-GL-JfPHCj3I/Tb_zrNTOZbI/AAAAAAAAABw/X8BByO04uSY/s1600/Eastern+Europe+Unicorn.jpg")
